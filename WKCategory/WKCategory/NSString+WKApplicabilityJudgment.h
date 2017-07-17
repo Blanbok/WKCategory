f719888f89d2dd6c9e00ba8540231796122dfa5c
Blanbok <a1653913692@icloud.com>
2017-07-17 09:27:58 +0800

    document change

--- WKCategory/WKCategory/NSString+WKApplicabilityJudgment.h
+++ WKCategory/WKCategory/NSString+WKApplicabilityJudgment.h
@@ -15,13 +15,20 @@
 #import <Foundation/Foundation.h>
 
 typedef NS_ENUM(NSInteger,WKApplicabilityJudgmentType){
-    WKApplicabilityJudgmentTypeNumberCharacters             = 1,        ///数字
-    WKApplicabilityJudgmentTypeOrdinaryEnglishCharacters    = 1 << 1,   ///英文
-    WKApplicabilityJudgmentTypeChineseCharacters            = 1 << 2,   ///中文
-    WKApplicabilityJudgmentTypeMobile               = 1 << 3,           ///电话
-    WKApplicabilityJudgmentTypeIDCard               = 1 << 4,           ///身份证
-    WKApplicabilityJudgmentTypeBankCard             = 1 << 5,           ///银行卡
-    WKApplicabilityJudgmentTypeEmail             = 1 << 6,              ///邮箱
+    ///数字
+    WKApplicabilityJudgmentTypeNumberCharacters             = 1,
+    ///英文
+    WKApplicabilityJudgmentTypeOrdinaryEnglishCharacters    = 1 << 1,
+    ///中文
+    WKApplicabilityJudgmentTypeChineseCharacters            = 1 << 2,
+    ///电话
+    WKApplicabilityJudgmentTypeMobile               = 1 << 3,
+    ///身份证
+    WKApplicabilityJudgmentTypeIDCard               = 1 << 4,
+    ///银行卡
+    WKApplicabilityJudgmentTypeBankCard             = 1 << 5,
+    ///邮箱
+    WKApplicabilityJudgmentTypeEmail             = 1 << 6,
 
 };
 @interface NSString (WKApplicabilityJudgment)
